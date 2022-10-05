2627957112fa94aca616e977d2f11ed02ecfd98f
Zied Ben Amor <zied-ci@hotmail.fr>
2022-10-05 20:53:04 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import "./style.css";
+const tasks = [
+  {
+    index: 0,
+    description: "Do the dishes",
+    completed: true,
+  },
+  {
+    index: 2,
+    description: "Do the laundry",
+    completed: false,
+  },
+  {
+    index: 1,
+    description: "Do the homework",
+    completed: false,
+  }];
 function component() {
-  const element = document.createElement("div");
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
+  const tasksSort = tasks.sort((a, b) => a.index - b.index);
+  const element = document.querySelector("#list");  
+  tasksSort.forEach((task) => { 
+    const taskElement = document.createElement("li");
+    taskElement.classList.add("items");
+    taskElement.innerHTML = 
+    
+      `
+          <div>
+            <input type='checkbox' ${task.completed ? 'checked' : ''}
+        class='box' name='car'>
+            <input for=''  class='task' id='task' value='${task.description}'>
+          </div>
+          <div>
+          <button class='remove-btn'>x</button>
+          </div>
+        `
+    
+    ;
+    element.appendChild(taskElement);
+   });
   return element;
 }
 
