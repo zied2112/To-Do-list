5990d8c99c1fe66353fb068011e399a943e3fb51
Zied Ben Amor <zied-ci@hotmail.fr>
2022-10-06 22:44:39 +0200

    Interactive list features

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
 import './style.css';
-import tasks from './data.js';
+import Todo from './main.js';
 
-const tasksList = tasks;
-
-const list = document.querySelector('#list');
-function component() {
-  const tasksSort = tasksList.sort((a, b) => a.index - b.index);
-  const element = document.createElement('ul');
-  tasksSort.forEach((task) => {
-    const taskElement = document.createElement('li');
-    taskElement.classList.add('items');
-    taskElement.innerHTML = `
-          <div>
-            <input type='checkbox' ${task.completed ? 'checked' : ''}
-        class='box' >
-            <input class='task' id='task' value='${task.description}'>
-          </div>
-          <div>
-          <button class='remove-btn'>x</button>
-          </div>
-        `;
-    element.appendChild(taskElement);
-  });
-  return element;
-}
-list.appendChild(component());
+const newtodo = new Todo();
+newtodo.value = '';
